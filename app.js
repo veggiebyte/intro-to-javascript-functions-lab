0b5e7b7e7bd766a8919d42a0990e231a1af38df5
“CindyS” <veggiebyte@yahoo.com>
2025-11-04 23:07:58 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,102 @@
+/*
+Exercise 1: maxOfTwoNumbers()
+
+In this exercise, create a function named maxOfTwoNumbers. 
+It should take two numbers as inputs and return the larger number. 
+If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
+// const maxOfTwoNumbers = (x, y) => {
+//   if (x >= y) {
+//     return x;
+//   } else {
+//     return y;
+//   }
+// }
+
+// console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+
+
+
+/*
+Exercise 2: isAdult()
+
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
+*/
+
+// function isAdult(age) {
+//   if (age >= 18) {
+//     return 'Adult';
+//   } else {
+//     return 'Minor';
+//   }
+// }
+
+// console.log('Exercise 2 Result:', isAdult(21));
+// console.log('Exercise 2 Result:', isAdult(17));
+// console.log('Exercise 2 Result:', isAdult(54));
+
+
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+// function isCharAVowel(char) {
+//   if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log('Exercise 3 Result:', isCharAVowel("a"));
+// console.log('Exercise 3 Result:', isCharAVowel("x"));
+// console.log('Exercise 3 Result:', isCharAVowel("c"));
+
+
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+function generateEmail(name, domain) {
+    return name + '@' + domain;
+}
+
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+
+
+
+
+
+
+
+
+
+
+
+
