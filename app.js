39175c814b5f060c0f5964b8bfec6505f9306af4
“CindyS” <veggiebyte@yahoo.com>
2025-11-09 12:43:54 -0800

    level up

--- app.js
+++ app.js
@@ -83,14 +83,251 @@ should return 'johnsmith@example.com'.
 Complete the exercise in the space below:
 */
 
-function generateEmail(name, domain) {
-    return name + '@' + domain;
+// function generateEmail(name, domain) {
+//     return name + '@' + domain;
+// }
+
+// console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+
+
+
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+
+// function greetUser(name, timeOfDay) {
+//   return `Good ${timeOfDay}, ${name}!`;
+// }
+
+// console.log('Exercise 5 Result:', greetUser('Sam', 'morning'));
+// console.log('Exercise 5 Result:', greetUser('Cindy', 'evening'));
+// console.log('Exercise 5 Result:', greetUser('Frank', 'afternoon'));
+
+
+
+/*
+Exercise 6: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers 
+and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
+
+// function maxOfThree(num1, num2, num3) {
+//   if (num1 >= num2 && num1 >= num3) {
+//     return num1;
+//   } else if (num2 >= num1 && num2 >= num3) {
+//     return num2;
+//   } else {
+//     return num3;
+//   }
+// }
+
+// console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
+// console.log('Exercise 6 Result:', maxOfThree(100, 900, 55));
+
+
+
+
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+
+// function calculateTip(bill, tip) {
+//     return (bill * tip / 100);
+// }
+
+
+// console.log('Exercise 7 Result:', calculateTip(50, 20));
+
+
+
+
+/*
+Exercise 8: convertTemperature()
+
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+
+// function convertTemperature(temp, scale) {
+//   if (scale == "C") {
+//     return temp * 1.8 + 32 + " F"
+//   }
+//   if (scale == "F") {
+//     return (temp - 32) / 1.8 + " C"
+//   }
+// }
+
+// console.log("Exercise 8 Result:", convertTemperature(32, "C"))
+// console.log("Exercise 8 Result:", convertTemperature(32, "F"))
+
+
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+
+// function basicCalculator(num1, num2, operation) {
+//   if (operation == "add") {
+//     return num1 + num2
+//   }
+//   if (operation == "subtract") {
+//     return num1 - num2
+//   }
+//   if (operation == "multiply") {
+//     return num1 * num2
+//   }
+//   if (operation == "divide") {
+//     return num1 / num2
+//   }
+// }
+
+// console.log("Exercise 9 Result:", basicCalculator(10, 5, "subtract"))
+// console.log("Exercise 9 Result:", basicCalculator(30, 25, "add"))
+// console.log("Exercise 9 Result:", basicCalculator(46, 22, "divide"))
+// console.log("Exercise 9 Result:", basicCalculator(50, 86, "multiply"))
+
+
+
+/*
+Exercise 10: calculateGrade()
+
+Define a function called calculateGrade. 
+It should take a numerical score and return the corresponding letter 
+grade (A, B, C, D, F). 
+
+For example, 90 and above yields an 'A', 80-89 is a 'B', 
+and 70-79 is a 'C', 60-69 is a 'D' and anything lower than a 60 is an 'F'.
+
+Example: calculateGrade(100) should return A.
+
+Complete the exercise in the space below:
+*/
+
+// function calculateGrade(numberGrade) {
+//   if (numberGrade >= 90) {
+//     return "A";
+//   }
+  
+//   if (numberGrade >= 80) {
+//     return "B";
+//   }
+
+//   if (numberGrade >= 70) {
+//     return "C";
+//   }
+
+//   if (numberGrade >= 60) {
+//     return "D";
+//   }
+
+//   if (numberGrade < 60) {
+//     return "F";
+//   }
+
+// }
+
+// console.log('Exercise 10 Result:', calculateGrade(85));
+
+// console.log('Exercise 10 Result:', calculateGrade(72));
+
+// console.log('Exercise 10 Result:', calculateGrade(92));
+
+// console.log('Exercise 10 Result:', calculateGrade(62));
+
+// console.log('Exercise 10 Result:', calculateGrade(42));
+
+
+//------------------------------------------------------------------divider between next exercise
+
+
+
+
+/*
+Exercise 11: createUsername()
+
+Define a function called createUsername. 
+It should take a first name and a last name and return a username. 
+
+The username should be a combination of the following:
+- The first three letters of the first name.
+- The first three letters of the last name.
+- The total character count of the first and last name combined.
+
+Example: createUsername('Samantha', 'Green') should return 'SamGre13'.
+
+Complete the exercise in the space below:
+*/
+
+// const createUsername = (firstName, lastName) =>{
+//   let firstThree = firstName.slice(0, 3);
+//   let lastThree = lastName.slice(0, 3);
+//   let totalLength = firstName.length + lastName.length;
+//   return firstThree + lastThree + totalLength;
+
+//   }
+
+// console.log('Exercise 11 Result:', createUsername("Samantha", "Green"));
+// console.log('Exercise 11 Result:', createUsername("Cindy", "Stadterman"));
+// console.log('Exercise 11 Result:', createUsername("Stehphen", "Morrissey"));
+
+
+
+
+//------------------------------------------------------------------divider between next exercise
+/*
+Exercise 12: numArgs()
+
+Challenge yourself with numArgs. 
+This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+*/
+function numArgs() {
+  return arguments.length;
 }
 
-console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
-
-
-
+console.log('Exercise 12 Result:', numArgs(1, 2, 3, 4));
+console.log('Exercise 12 Result:', numArgs(15000, 6595));
+console.log('Exercise 12 Result:', numArgs(62, 85, 247));
 
 
 
